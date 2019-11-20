03be7532d52dc2e206533514539343d82f7f0a47
Balazs Ankes <balazs.ankes@gmail.com>
2019-11-20 03:03:08 +0100

    feat(main-map): introduce component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-map/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMap from './components/main-map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'main-map': MainMap
   }
 }
 </script>
---
+++ src/components/main-map.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="my-map">
+
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+
+export default {
+  name: 'MainMap',
+  data () {
+    return {
+      map: null
+    }
+  },
+  props: {
+    msg: String
+  },
+  mounted () {
+    mapboxgl.accessToken = ''
+
+    this.map = new mapboxgl.Map({
+      container: 'my-map',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#my-map {
+  height: 400px;
+  width: 400px;
+}
+</style>
