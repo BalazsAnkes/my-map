dd9de37da485efd695878de1294234b4e37de013
Balazs Ankes <balazs.ankes@gmail.com>
2019-11-25 00:10:33 +0100

    feat(travel-guide): create component

---
+++ src/components/travel-guide.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="guide">
+    <ul>
+      <li>Distance: {{distance}}m</li>
+      <li>Duration: {{duration}}s</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TravelGuide',
+  props: {
+    travelData: {
+      type: Object,
+      default () {
+        return {
+          distance: 0,
+          duration: 0
+        }
+      }
+    }
+  },
+  computed: {
+    distance () { return this.travelData.distance },
+    duration () { return this.travelData.duration }
+  }
+}
+</script>
+
+<style scoped>
+#guide {
+  background: #fff;
+  left: 10px;
+  position: absolute;
+  top: 10px;
+  display: none;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+ }
+
+ li {
+   box-sizing: border-box;
+   padding: 10px;
+ }
+</style>
